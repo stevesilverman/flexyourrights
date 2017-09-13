cdc2eff7a4af0a25e144f066d8857e90dd0df0e6
Ben Word <ben@benword.com>
2017-09-12 19:11:18 -0600

    Update WP and plugins

--- site/web/app/themes/flexyourrights/inc/template-tags.php
+++ site/web/app/themes/flexyourrights/inc/template-tags.php
@@ -21,17 +21,4 @@ function roots_entry_meta() {
       echo '<p class="byline">Written by ' . $story_name . $story_location . '</p>';
     }
   }
-
-  // Share Buttons
-  if (!is_front_page()) {
-    echo '<div class="share-container">';
-    global $post;
-    $url = urlencode(get_permalink( $post->ID ));
-    $title = the_title('', '', false);
-    echo '<div class="share share-facebook"><iframe src="https://www.facebook.com/plugins/like.php?href='. $url .'&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:20px;" allowTransparency="true"></iframe></div>';
-    echo '<div class="share share-twitter"><iframe allowtransparency="true" frameborder="0" scrolling="no" src="https://platform.twitter.com/widgets/tweet_button.html?url='. $url .'&text='. $title .'&count=horizontal&via=flexyourrights&related=flexyourrights" style="width:110px; height:20px;"></iframe></div>';
-    echo '<div class="share share-reddit"><script src="https://www.reddit.com/buttonlite.js?i=2&url='. $url .'"></script></div>';
-    echo '</div>';
-  }
-
 }
